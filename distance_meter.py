036aeca04e4bd2172b9402b9af0a4028638b8e48
Robert Gruberski <rgruberski@gmail.com>
2016-01-20 13:02:44 +0100

    methods for distance calculations

--- distance_meter.py
+++ distance_meter.py
@@ -1,2 +1,12 @@
 class DistanceMeter:
-    pass
+    def __init__(self, initial_edge_size, initial_distance):
+        self.initial_edge_size = initial_edge_size
+        self.initial_distance = initial_distance
+
+        self.focal_length = 0
+
+    def setup(self, width):
+        self.focal_length = (width * self.initial_distance) / self.initial_edge_size
+
+    def distance(self, read_width):
+        return (self.initial_edge_size * self.focal_length) / read_width
